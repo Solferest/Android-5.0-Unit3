9724980a40d26d61d806da33b2c0169f94353f1f
Solferest <112902148+Solferest@users.noreply.github.com>
2024-03-02 23:16:16 +0300

    Create MentalRep.kt

---
+++ MentalRep.kt
@@ -0,0 +1,159 @@
+package com.example.a30daysapplication.model
+import com.example.a30daysapplication.R
+
+object MentalRep {
+    val mentalTips = listOf(
+        MentalTip(
+            nameRes = R.string.tip_1,
+            instructionRes = R.string.tip_1_d,
+            imageRes = R.drawable.tip_1_image,
+        ),
+        MentalTip(
+            nameRes = R.string.tip_2,
+            instructionRes = R.string.tip_2_d,
+            imageRes = R.drawable.tip_2_image,
+        ),
+        MentalTip(
+            nameRes = R.string.tip_3,
+            instructionRes = R.string.tip_3_d,
+            imageRes = R.drawable.tip_3_image,
+        ),
+        MentalTip(
+            nameRes = R.string.tip_4,
+            instructionRes = R.string.tip_4_d,
+            imageRes = R.drawable.tip_4_image,
+        ),
+        MentalTip(
+            nameRes = R.string.tip_5,
+            instructionRes = R.string.tip_5_d,
+            imageRes = R.drawable.tip_5_image,
+        ),
+        MentalTip(
+            nameRes = R.string.tip_6,
+            instructionRes = R.string.tip_6_d,
+            imageRes = R.drawable.tip_6_image,
+        ),
+        MentalTip(
+            nameRes = R.string.tip_7,
+            instructionRes = R.string.tip_7_d,
+            imageRes = R.drawable.tip_7_image,
+        ),
+        MentalTip(
+            nameRes = R.string.tip_8,
+            instructionRes = R.string.tip_8_d,
+            imageRes = R.drawable.tip_8_image,
+        ),
+        MentalTip(
+            nameRes = R.string.tip_9,
+            instructionRes = R.string.tip_9_d,
+            imageRes = R.drawable.tip_9_image,
+        ),
+        MentalTip(
+            nameRes = R.string.tip_10,
+            instructionRes = R.string.tip_10_d,
+            imageRes = R.drawable.tip_10_image,
+        ),
+        MentalTip(
+            nameRes = R.string.tip_11,
+            instructionRes = R.string.tip_11_d,
+            imageRes = R.drawable.tip_11_image,
+        ),
+        MentalTip(
+            nameRes = R.string.tip_12,
+            instructionRes = R.string.tip_12_d,
+            imageRes = R.drawable.tip_12_image,
+        ),
+        MentalTip(
+            nameRes = R.string.tip_13,
+            instructionRes = R.string.tip_13_d,
+            imageRes = R.drawable.tip_13_image,
+        ),
+        MentalTip(
+            nameRes = R.string.tip_14,
+            instructionRes = R.string.tip_14_d,
+            imageRes = R.drawable.tip_14_image,
+        ),
+        MentalTip(
+            nameRes = R.string.tip_15,
+            instructionRes = R.string.tip_15_d,
+            imageRes = R.drawable.tip_15_image,
+        ),
+        MentalTip(
+            nameRes = R.string.tip_16,
+            instructionRes = R.string.tip_16_d,
+            imageRes = R.drawable.tip_16_image,
+        ),
+        MentalTip(
+            nameRes = R.string.tip_17,
+            instructionRes = R.string.tip_17_d,
+            imageRes = R.drawable.tip_17_image,
+        ),
+        MentalTip(
+            nameRes = R.string.tip_18,
+            instructionRes = R.string.tip_18_d,
+            imageRes = R.drawable.tip_18_image,
+        ),
+        MentalTip(
+            nameRes = R.string.tip_19,
+            instructionRes = R.string.tip_19_d,
+            imageRes = R.drawable.tip_19_image,
+        ),
+        MentalTip(
+            nameRes = R.string.tip_20,
+            instructionRes = R.string.tip_20_d,
+            imageRes = R.drawable.tip_20_image,
+        ),
+        MentalTip(
+            nameRes = R.string.tip_21,
+            instructionRes = R.string.tip_21_d,
+            imageRes = R.drawable.tip_21_image,
+        ),
+        MentalTip(
+            nameRes = R.string.tip_22,
+            instructionRes = R.string.tip_22_d,
+            imageRes = R.drawable.tip_22_image,
+        ),
+        MentalTip(
+            nameRes = R.string.tip_23,
+            instructionRes = R.string.tip_23_d,
+            imageRes = R.drawable.tip_23_image,
+        ),
+        MentalTip(
+            nameRes = R.string.tip_24,
+            instructionRes = R.string.tip_24_d,
+            imageRes = R.drawable.tip_24_image,
+        ),
+        MentalTip(
+            nameRes = R.string.tip_25,
+            instructionRes = R.string.tip_25_d,
+            imageRes = R.drawable.tip_25_image,
+        ),
+        MentalTip(
+            nameRes = R.string.tip_26,
+            instructionRes = R.string.tip_26_d,
+            imageRes = R.drawable.tip_26_image,
+        ),
+        MentalTip(
+            nameRes = R.string.tip_27,
+            instructionRes = R.string.tip_27_d,
+            imageRes = R.drawable.tip_27_image,
+        ),
+        MentalTip(
+            nameRes = R.string.tip_28,
+            instructionRes = R.string.tip_28_d,
+            imageRes = R.drawable.tip_28_image,
+        ),
+        MentalTip(
+            nameRes = R.string.tip_29,
+            instructionRes = R.string.tip_29_d,
+            imageRes = R.drawable.tip_29_image,
+        ),
+        MentalTip(
+            nameRes = R.string.tip_30,
+            instructionRes = R.string.tip_30_d,
+            imageRes = R.drawable.tip_30_image,
+        ),
+    )
+
+
+}
